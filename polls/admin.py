ff88c1c22c6d38271cd3a147df56d870f652c8cb
takeshi-itami <tksh.itm@gmail.com>
2019-04-18 23:17:30 +0900

    add list_display for QuestionAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -19,6 +19,7 @@ class QuestionAdmin(admin.ModelAdmin):
             },
         ),
     ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 
